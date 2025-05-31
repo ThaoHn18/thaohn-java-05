c18a362b714f5dd8e03bb3418f39734457b43e85
ThaoHN <hoangngocthaonazca@baoviet.com.vn>
2025-05-31 21:47:04 +0700

    cau hinh cors

---
+++ src/main/java/thaohn/thaohn_sample_code/configuration/AppConfig.java
@@ -0,0 +1,15 @@
+package thaohn.thaohn_sample_code.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true).allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+    }
+}
+
